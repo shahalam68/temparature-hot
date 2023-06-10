29c385d768047a195f354b325ee30ca1dbe12d10
Soykot Alam <a.k.mshahalam68@gmail.com>
2023-06-10 11:19:06 +0600

    first commit

--- js/app.js
+++ js/app.js
@@ -5,10 +5,25 @@ const loadTemperature = city =>{
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
     fetch(url)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => displayTemperature(data))
 }
-loadTemperature('Dhaka')
 
 const displayTemperature = data =>{
-    const temperature = document.getElementById()
+    // // const temperature = document.getElementById('temperature');
+    // // console.log(data.main.temp);
+    // temperature.innerText = data.main.temp;
+    setInnerTextById('temperature', data.main.temp)
+    setInnerTextById('condition', data.weather[0].main);
 }
+const setInnerTextById = (id,text)=>{
+    const temperature = document.getElementById(id);
+    temperature.innerText = text;
+}
+document.getElementById('btn-search').addEventListener('click', function(){
+    const searchField = document.getElementById('search-field');
+    const city = searchField.value;
+    document.getElementById('city').innerText = city
+    loadTemperature(city);
+})
+
+loadTemperature('Dhaka')
